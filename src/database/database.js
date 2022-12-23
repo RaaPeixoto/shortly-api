91763cec5feac71e67b3832542ebc7e20c486fff
Raísa Peixoto <raisacad.engcivil@gmail.com>
2022-12-23 00:00:31 -0300

    fix: connection para deploy

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
 import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Pool } = pkg;
-dotenv.config();
- const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  
-  });
 
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 
   export default connection;
